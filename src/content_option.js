af9a8ce8c376afeb84d5522cce49a632820f291e
Saudie11 <saudshaikh800@gmail.com>
2024-09-29 17:38:46 +0530

    Prepare for GitHub Pages deployment

---
+++ src/content_option.js
@@ -0,0 +1,133 @@
+
+const logotext = "SAUD SHAIKH";
+
+
+const meta = {
+    title: "Saud Shaikh",
+    description: "I’m Saud Shaikh  _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Saud Shaikh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop React apps",
+    },
+    description: "Hardworking and resourceful individual committed to building user-friendly, functional and sustainable web applications. Highly skilled in project management and multitasking to deliver complex projects within tight deadlines.",
+    your_img_url: "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "Apart from coding, I enjoy playing football and am a keen follower of cricket. These activities help me stay active and enhance my teamwork and problem-solving skills.",
+};
+const worktimeline = [{
+        jobtitle: "Product Promoter",
+        where: "Quess Crop",
+        year: "2018",
+    },
+    {
+        jobtitle: "Customer Service Executive (CSE)",
+        where: "Teleperformance",
+        year: "2021",
+    },
+    {
+        jobtitle: "Team Leader",
+        where: "Teleperformance",
+        year: "2022",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Mongo Db",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "JavaScript",
+        description: "I would discribe Myself as a learner for Javascript and asspired to be an expert ",
+    },
+    {
+        title: "React Apps",
+        description: "Have build small frontend projects and look forward to build more",
+    },
+    {
+        title: "Node js",
+        description: " A basics of node express and mongodb helps to create an api and Crud .  ",
+    },
+];
+
+const dataportfolio = [{
+        img :"https://cloud2data.com/wp-content/uploads/2023/01/HTML-CSS-Review.png",
+        description: "A simple website of 5 pages created with HTML & Css",
+        link: "https://saudie11.github.io/Bingo/",
+    },
+    {
+        img: "https://shethink.in/wp-content/uploads/2021/07/react.js-img.png",
+        description: "Motors a ecommerce Frontend Project created by Reactjs .",
+        link: "https://saudie11.github.io/Motors/",
+    },
+    {
+        img: "https://cms.involve.me/blog/how-to-create-a-simple-price-calculator-and-capture-more-leads/calculator-L.png",
+        description: "The React js Calculator",
+        link: "https://saudie11.github.io/mycalculater-react/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "saudshaikh800@gmail.com",
+    YOUR_FONE: "7208353411",
+    description: "Hardworking and resourceful individual committed to building user-friendly, functional and sustainable web applications. Highly skilled in project management and multitasking to deliver complex projects within tight deadlines. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Saudie11",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
